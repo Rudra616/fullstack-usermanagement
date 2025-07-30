28a169a72f2f7da7e6e0e93fa83806ab6e60aa6f
rudra <rudrampanchal@gmail.com>
2025-07-30 19:17:14 +0530

    add models,serilaizer,and also class(genrics)

--- backend-django/userManagement/app/urls.py
+++ backend-django/userManagement/app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
+from .views import userListCrateGenric
 
 urlpatterns = [
-    
+    path('users/',userListCrateGenric.as_view())
 ]
--- backend-django/userManagement/app/views.py
+++ backend-django/userManagement/app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .serilaizers import UserSerilaizer
+from .models import User
+from rest_framework import generics
+
+
+class userListCrateGenric(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerilaizer 
